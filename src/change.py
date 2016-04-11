66c66d04831eb8a8a117810114e9c2485760bd46
Abhinav Deep Singh <Abhinav@Krakens-Macbook-Pro.local>
2016-04-11 22:56:38 +0530

    New files

---
+++ src/change.py
@@ -0,0 +1,13 @@
+with open("../svm/data_new.train",'r') as f: 
+	for line in f:
+		a = line.strip().split()
+		b = [a[0]]
+		c = a[1:]
+		for i in range(0,len(c)):
+			m,n = c[i].split(':')
+			if(int(m) < 9):
+				n = '1.0000000000000000'
+			c[i] = m+":"+n
+		b.extend(c)
+		a = ' '.join(b)
+		print a 
